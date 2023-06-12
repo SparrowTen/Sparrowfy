2456768324dd692b60bce17d87ba27bdca3afe22
Poyu <42506064+poyu39@users.noreply.github.com>
2023-06-13 02:31:07 +0800

    新增 auth.py

--- API/python/main.py
+++ API/python/main.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 
 from song import song_blp
+from auth import auth_blp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(song_blp, url_prefix = '/api/song/')
+app.register_blueprint(auth_blp, url_prefix = '/api/auth/')
 
 if __name__ == '__main__':
-    app.run(debug=True,host="127.0.0.1",port=5500)
+    app.run(debug=True,host="127.0.0.1",port=5000)
