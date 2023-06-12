6d8c449da1e8b1c616a35ae09e34d557590604c9
Poyu <42506064+poyu39@users.noreply.github.com>
2023-06-13 02:34:15 +0800

    新增使用範例

--- API/python/main.py
+++ API/python/main.py
@@ -12,3 +12,25 @@ app.register_blueprint(auth_blp, url_prefix = '/api/auth/')
 
 if __name__ == '__main__':
     app.run(debug=True,host="127.0.0.1",port=5000)
+    
+'''
+```
+GET:
+  getID
+  127.0.0.1:5000/api/song/getID?id=1
+
+  getPlayList
+  127.0.0.1:5000/api/song/getPlayList?artist=Ayase / YOASOBI
+
+POST:
+  login
+  127.0.0.1:5000/api/auth/login
+  - name
+  - passowrd
+
+  register
+  127.0.0.1:5000/api/auth/register
+  - name
+  - password
+```
+'''
